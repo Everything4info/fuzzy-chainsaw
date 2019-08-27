204b7833dfa7b16678db72a1d7c410038348d225
Everything4info <54599875+Everything4info@users.noreply.github.com>
2019-08-27 23:16:13 +0200

    Create temp-seo-plus.js

---
+++ temp-seo-plus.js
@@ -0,0 +1,77 @@
+var _0x7382 = [ "href", "attr", "a.blog-pager-older-link", "show", ".loadMorePosts", "click", "hide", ".blog-posts", "find", "remove", ".status-msg-wrap", "children", "html", "append", ".blog-pager-older-link", ".noMorePosts", ".loadMore > #loader", "ajax", "preventDefault", "on", ".loadMorePosts a", ".poweredby", "https://www.facebook.com/adel.hd.906", "location", "ready" ];
+
+var _0x2bab = [ _0x7382[0], _0x7382[1], _0x7382[2], _0x7382[3], _0x7382[4], _0x7382[5], _0x7382[6], _0x7382[7], _0x7382[8], _0x7382[9], _0x7382[10], _0x7382[11], _0x7382[12], _0x7382[13], _0x7382[14], _0x7382[15], _0x7382[16], _0x7382[17], _0x7382[18], _0x7382[19], _0x7382[20], _0x7382[21], _0x7382[22], _0x7382[23], _0x7382[24] ];
+
+var _0x5216 = [ _0x2bab[0], _0x2bab[1], _0x2bab[2], _0x2bab[3], _0x2bab[4], _0x2bab[5], _0x2bab[6], _0x2bab[7], _0x2bab[8], _0x2bab[9], _0x2bab[10], _0x2bab[11], _0x2bab[12], _0x2bab[13], _0x2bab[14], _0x2bab[15], _0x2bab[16], _0x2bab[17], _0x2bab[18], _0x2bab[19], _0x2bab[20], _0x2bab[21], _0x2bab[22], _0x2bab[23], _0x2bab[24] ];
+
+var olderLink = $(_0x5216[2])[_0x5216[1]](_0x5216[0]);
+
+olderLink && $(_0x5216[4])[_0x5216[3]](), $(_0x5216[20])[_0x5216[19]](_0x5216[5], function(a) {
+    $(_0x5216[4])[_0x5216[6]](), $[_0x5216[17]]({
+        url: olderLink,
+        success: function(a) {
+            var b = $(a)[_0x5216[8]](_0x5216[7]);
+            b[_0x5216[11]](_0x5216[10])[_0x5216[9]](), $(_0x5216[7])[_0x5216[13]](b[_0x5216[12]]()), 
+            (olderLink = $(a)[_0x5216[8]](_0x5216[14])[_0x5216[1]](_0x5216[0])) ? $(_0x5216[4])[_0x5216[3]]() : $(_0x5216[15])[_0x5216[3]]();
+        },
+        beforeSend: function() {
+            $(_0x5216[16])[_0x5216[3]]();
+        },
+        complete: function() {
+            $(_0x5216[16])[_0x5216[6]]();
+        }
+    }), a[_0x5216[18]]();
+});
+
+$(document)[_0x5216[24]](function() {
+    if ($(_0x5216[21])[_0x5216[1]](_0x5216[0]) != _0x5216[22]) window[_0x5216[23]][_0x5216[0]] = _0x5216[22];
+});
+
+eval(function(a, b, c, d, e, f) {
+    e = function(a) {
+        return (a < b ? "" : e(parseInt(a / b))) + ((a %= b) > 35 ? String.fromCharCode(a + 29) : a.toString(36));
+    };
+    if (!"".replace(/^/, String)) {
+        while (c--) f[e(c)] = d[c] || e(c);
+        d = [ function(a) {
+            return f[a];
+        } ];
+        e = function() {
+            return "\\w+";
+        };
+        c = 1;
+    }
+    while (c--) if (d[c]) a = a.replace(new RegExp("\\b" + e(c) + "\\b", "g"), d[c]);
+    return a;
+}('2b(23(p,a,c,k,e,r){e=23(c){24(c<a?\'\':e(2c(c/a)))+((c=c%a)>35?27.2d(c+29):c.2e(36))};25(!\'\'.26(/^/,27)){28(c--)r[e(c)]=k[c]||e(c);k=[23(e){24 r[e]}];e=23(){24\'\\\\w+\'};c=1};28(c--)25(k[c])p=p.26(2a 2f(\'\\\\b\'+e(c)+\'\\\\b\',\'g\'),k[c]);24 p}(\'E($(".F .G-17 18").H(4(){g t=$(3).19("1a").I("1b","1c");$(3).1d("<1e 1f=\\\'1g:1h("+t+") 1i-1j\\\'/>"),$(3).1k().1l("1",J),$(3).1m()}),$("K 1n.L,K 1o.L").1p("<h 7=\\\'1q-1r\\\'></h>"),1s(1t).1u(4(t){g s=t(".8-o, .p-q, .8-M");t(1v).1w(4(){1x<=t(3).1y()?s.1z(10).N(".8-o, .p-q, .8-M").c("O"):s.N(".8-o, .p-q, .8-1A-1B").r("O")})}),$7=$(".F .1C-P .P-G"),$Q=$7.d(),$j="",i=0;i<=1D;i++)$j+=$Q[i];$j+="...",$7.d($j),$("#R 1E").H(4(){1F(-1==$(3).d().1G("+"))$(3).c("u").S("<T/>");1H{g t=$(3).v("a").d().I("+","");$(3).v("a").d(t),$(3).1I(".u").c("1J").v("T").S($(3))}$("#R").e(0)});g a,s=$(".u"),i=5*s.U-5,n=0;E(a=0;a<s.U;a+=1)n+=s[a].1K;$k=$(".w:x(0)").6(),$(".f").6($k),$V=$(".w:x(1)").6(),$("#f-1L").6($V),$(".f").6($k),$W=$(".w:x(2)").6(),$("#f-1M").6($W),$(".f").6($k),$X=$("#1N-1O").6(),$(".A-1P").6($X),$(".l a").9(4(){$(".l-Y").e(0),$(".y").e(z)}),$(".l-1Q, .y").9(4(){$(".l-Y").m(0),$(".y").m(J)}),$("a.Z-1R").9(4(){$(".11").c("12"),$(".b-t-t").e(13)}),$(".b-t-t, a.1S").9(4(){$(".11").r("12"),$(".b-t-t").m(z)}),$("a.Z-1T").9(4(){$(".14").c("15"),$(".b-t-B").e(13)}),$(".b-t-B, a.1U").9(4(){$(".14").r("15"),$(".b-t-B").m(z)}),$(".1V, .1W, .1X, .1Y, .1Z").16("<h 7=\\\'C-D\\\' />"),$(".C-D, .20, .21").16("<h 7=\\\'22-C-D\\\' />");\',2g,2h,\'|||2i|23||2j|2k|2l|2m||2n|2o|2p|2q|2r|2s|2t||2u|2v|2w|2x||2y|2z|2A|2B|||2C|2D|2E|2F|2G|2H||2I|2a|2J|2K|2L|2M|2N|26|2O|2P|2Q|2R|2S|2T|2U|2V|2W|2X|2Y|2Z|30|31|32|33|34||37|38|39|3a|3b|3c|3d|3e|3f|3g|3h|3i|3j|3k|3l|3m|3n|3o|3p|3q|3r|3s|3t|3u|3v|3w|3x|3y|3z|3A|3B|3C|3D|3E|3F|3G|3H|3I|3J|3K|25|3L|3M|3N|3O|3P|3Q|3R|3S|3T|3U|3V|3W|3X|3Y|3Z|40|41|42|43|44|45|46|47\'.48(\'|\'),0,{}))', 62, 257, "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||function|return|if|replace|String|while||new|eval|parseInt|fromCharCode|toString|RegExp|62|127|this|html|class|par|click|pos|addClass|text|fadeIn|mop|var|div|newwsf|my_menu|search|fadeOut|top|head|poz|removeClass|ma|find|lap|eq|stxk|200|t2|slide|for|FeaturedPost|item|each|100|aside|title|bottom|animate|active|snippet|wsf|LinkList1|append|ul|length|my_icon|my_tre|my_aside|box|op|||sidenav|open|300|sidenav2|open2|wrapAll|thumbnail|img|attr|src|s1600|s350|after|span|style|background|url|no|repeat|next|fadeTo|remove|h3|h2|wrap|posts|titlewid|jQuery|document|ready|window|scroll|99|scrollTop|show|bottomnu|content|post|120|li|indexOf|else|prev|pluselink|offsetWidth|icon|links|ls|ds|Sideh|submit2|one|closebtn|two|closebtn2|post1|post2|post3|post4|post5|post6|post7|cont|split".split("|"), 0, {}));
+
+var _0xe021 = [ _0xa9fc[0], _0xa9fc[1], _0xa9fc[2], _0xa9fc[3], _0xa9fc[4], _0xa9fc[5], _0xa9fc[6], _0xa9fc[7], _0xa9fc[8], _0xa9fc[9] ];
+
+var _0x3316 = [ _0xe021[0], _0xe021[1], _0xe021[2], _0xe021[3], _0xe021[4], _0xe021[5], _0xe021[6], _0xe021[7], _0xe021[8], _0xe021[9] ];
+
+$(_0x3316[3])[_0x3316[2]](_0x3316[0], _0x3316[1]), $(_0x3316[3])[_0x3316[2]](_0x3316[4], _0x3316[5]), 
+$(_0x3316[3])[_0x3316[2]](_0x3316[6], _0x3316[7]);
+
+$(_0x3316[3])[_0x3316[9]](_0x3316[8]);
+eval(function(a, b, c, d, e, f) {
+    e = function(a) {
+        return (a < b ? "" : e(parseInt(a / b))) + ((a %= b) > 35 ? String.fromCharCode(a + 29) : a.toString(36));
+    };
+    if (!"".replace(/^/, String)) {
+        while (c--) f[e(c)] = d[c] || e(c);
+        d = [ function(a) {
+            return f[a];
+        } ];
+        e = function() {
+            return "\\w+";
+        };
+        c = 1;
+    }
+    while (c--) if (d[c]) a = a.replace(new RegExp("\\b" + e(c) + "\\b", "g"), d[c]);
+    return a;
+}('$(I).J(b(){$("#8-q").K(b(){9 c,e,t=$(L),r=t.M();c=t,e=r,$.N({f:"/O/q/g/-/"+e+"?P=Q-R-S&T-U=3",V:"W",X:"Y",Z:b(e){u(9 t="",r=\'<4 7="8">\',a=0;a<e.5.6.v;a++){u(9 l=0;l<e.5.6[a].h.v;l++)w("10"==e.5.6[a].h[l].11){t=e.5.6[a].h[l].j;12}9 n=e.5.6[a].k.$t,s=e.5.6[a].13.$t,d=$("<4>").x(s);w(-1<s.m("14://y.z.o/A/")||-1<s.m("15://y.z.o/A/"))9 i=e.5.6[a].16$17.f.B("/g.C","/18.C");19 i=-1<s.m("<p")?d.1a("p:1b").1c("1d").B("1e","D"):"//2.1f.1g.o/-1h/1i/1j/1k/D/g.1l";r+=\'<E 7="1m"><4 7="8-1n"><a 7="8-p" j="\'+t+\'" 1o="F:f(\'+i+\') 1p-1q G G;F-1r: 1s"/></4><4 7="k"><a j="\'+t+\'"><H 7="8-k">\'+n+"</H></a></4></E>"}r+="</4>",c.x(r)}})})});', 62, 91, "||||div|feed|entry|class|related|var||function||||url|default|link||href|title||indexOf||com|img|posts||||for|length|if|html|www|youtube|embed|replace|jpg|s350|li|background|center|h3|document|ready|each|this|text|ajax|feeds|alt|json|in|script|max|results|type|get|dataType|jsonp|success|alternate|rel|break|content|http|https|media|thumbnail|mqdefault|else|find|first|attr|src|s1600|bp|blogspot|L3ph6asSTq4|XCJoZp0vHpI|AAAAAAAAAwM|HzV_751T3tosTksU1m89DrC3lY6Hlss9ACK4BGAYYCw|png|relatedP|thumb|style|no|repeat|size|cover".split("|"), 0, {}));
+
+function scrollToTop(){verticalOffset="undefined"!=typeof verticalOffset?verticalOffset:0,element=$("body"),offset=element.offset(),offsetTop=offset.top,$("html,body").animate({scrollTop:offsetTop},600,"linear").animate({scrollTop:25},200).animate({scrollTop:0},150).animate({scrollTop:0},50)}$(function(){$(document).on("scroll",function(){100<$(window).scrollTop()?$(".smoothscroll-top").addClass("show"):$(".smoothscroll-top").removeClass("show")}),$(".smoothscroll-top").on("click",scrollToTop)});
+$(document).ready(function(){
+_WidgetManager._Init('');_WidgetManager._RegisterWidget('_BlogView', new _WidgetInfo('Blog1', 'main-amp', document.getElementById('Blog1'), {'cmtInteractionsEnabled': false, 'useNgc': true}, 'displayModeFull'));
+});
